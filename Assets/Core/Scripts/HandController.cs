669df7953b12628b061ea38b5390cbc03366310c
L4 VR <l4-vr@l4digital.com>
2017-02-02 17:01:31 -0800

    Added scripts to control wands, began work on ball scene

---
+++ Assets/Core/Scripts/HandController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandController : MonoBehaviour {
+
+	public GameObject cube;
+	public Color change;
+
+	private GameObject inside;
+	private FixedJoint holding;
+
+	private ViveWandController controller;
+
+	void Start () {
+		controller = GetComponent<ViveWandController> ();
+	}
+
+	void FixedUpdate () {
+		if (controller.trigger_down) {
+			cube.GetComponent<Renderer>().material.color = change;
+		}
+
+		if (inside != null && controller.trigger_down) {
+			holding = inside.AddComponent<FixedJoint> ();
+			holding.connectedBody = GetComponent<Rigidbody> ();
+			print ("picked up object");
+		}
+
+		if (holding != null && controller.trigger_down) {
+			//Object.DestroyImmediate (holding);
+			//holding = null;
+		}
+			
+	}
+
+	private void OnTriggerEnter(Collider coll) {
+		inside = coll.gameObject;
+	}
+
+	private void OnTriggerExit(Collider coll) {
+		inside = null;
+	}
+}
---
+++ Assets/Core/Scripts/ViveWandController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ViveWandController : MonoBehaviour {
+
+	// Variables for various controller buttons/states
+	private Valve.VR.EVRButtonId grip_id = Valve.VR.EVRButtonId.k_EButton_Grip;
+	public bool grip_down = false;
+	public bool grip_up = false;
+	public bool grip_pressed = false;
+
+	private Valve.VR.EVRButtonId trigger_id = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+	public bool trigger_down = false;
+	public bool trigger_up = false;
+	public bool trigger_pressed = false;
+
+	// Variables that link to which controller this script controls
+	private SteamVR_TrackedObject trackedObj;
+	private SteamVR_Controller.Device controller;
+
+	void Start () {
+		trackedObj = GetComponent<SteamVR_TrackedObject>();
+		controller = SteamVR_Controller.Input ((int)trackedObj.index);
+	}
+
+	void FixedUpdate () {
+		if (controller != null) {
+			// Update variables to current controller state
+			grip_down = controller.GetPressDown (grip_id);
+			grip_up = controller.GetPressUp (grip_id);
+			grip_pressed = controller.GetPress (grip_id);
+
+			trigger_down = controller.GetPressDown (trigger_id);
+			trigger_up = controller.GetPressUp (trigger_id);
+			trigger_pressed = controller.GetPress (trigger_id);
+		} else {
+			print ("Controller not initialized");
+		}
+	}
+}
